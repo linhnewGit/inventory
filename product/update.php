a132915dc6a3ac6eee086790d352338492f1ceb5
linhnewGit <125236387+linhnewGit@users.noreply.github.com>
2023-03-07 15:57:59 +0700

    update

--- product/update.php
+++ product/update.php
@@ -2,24 +2,24 @@
     $id = $_GET['id'];
     $sql_category = "SELECT * FROM category";
     $query_category = mysqli_query($connect, $sql_category);
-    $sql_up = "SELECT * FROM product where product";
+    $sql_up = "SELECT * FROM product where p_id=$id";
     $query_up = mysqli_query($connect, $sql_up);
     $row_up = mysqli_fetch_assoc($query_up);
 
     if(isset($_POST['sbm'])){
         $p_name = $_POST['p_name'];
         if($_FILES['image']['name']==''){
-            $image = 
+            $image = $row_up['p_name'];
+        }else{
+            $image = $row_up['p_name'];
         }
         
-        $image_tmp = $_FILES['image']['tmp_name'];
         $price = $_POST['price'];
         $quantity = $_POST['quantity'];
         $category_id = $_POST['category_id'];
         $sql = "INSERT INTO product (p_name, image, price, quantity, category_id)
         VALUE ('$p_name', '$image', $price, $quantity, $category_id)";
         $query = mysqli_query($connect, $sql);
-        moce_upload_file($image_tmp, 'img/'.$image);
         header('location: index.php?page_layout=list');
     }
 ?>
@@ -27,13 +27,13 @@
 <div class="container-fluid">
     <div class="card">
         <div class="card-header">
-            <h2>Add new inventory</h2>
+            <h2>Update product</h2>
         </div>
         <div class="card-body">
             <form method="POST" enctype="multipart/form-data">
                 <div class="form-group">
                   <label for="">Product name</label>
-                  <input type="text" name="p_name" class="form-control" required>
+                  <input type="text" name="p_name" class="form-control" required value="<?php echo $row_up['p_name']; ?>">
                 </div>
 
                 <div class="form-group">
